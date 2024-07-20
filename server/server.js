1634ec9da92986058eb4964fe852656d4f65b259
shray_gla_git <shray.gupta_cs21@gla.ac.in>
2024-07-20 11:36:30 +0530

    second

--- server/server.js
+++ server/server.js
@@ -3,24 +3,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-// Inititating Express
 const app = express();
 
-// Environment Variables
 require("dotenv").config();
 
-// Connecting to Database
-mongoose
-  .connect(process.env.dbURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then((result) =>
-    app.listen(process.env.PORT || 3000, () => {
-      console.log("Connection to the Database was established!");
-    })
-  )
-  .catch((error) => console.log(error));
+
 
 // Middlewares
 app.use(express.json()); // JSON Parser
